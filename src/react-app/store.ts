43d0a2d78a1a8d62d88d7d099fa93cd9c67c17c0
Mark <doylethecat@gmail.com>
2026-04-19 18:34:22 -0400

    [CODE] Add high-level species and reactions

--- src/react-app/store.ts
+++ src/react-app/store.ts
@@ -10,22 +10,42 @@ import {
   type OnConnect,
 } from '@xyflow/react';
 
+
+type species = {
+  id: string;
+  name: string;
+  initial: number;
+}
+
+type reactions = {
+  id: string;
+  name: string;
+  from: string[];
+  to: string[];
+}
+
 const initialNodes: AppNode[] = [
-  // { id: 'n0', position: { x: -500, y: -500 }, data: { label: 'DEFAULT NODE (ERROR)', onLabelChange: () => {}, color: '#ddd', initial: '' }, type: 'protein'},
   { id: 'Na', position: { x: 0, y: -0 }, data: { label: 'Click to edit', onLabelChange: () => {}, color: '#90f1ef', initial: '' }, type: 'protein'},
   { id: 'Nb', position: { x: 500, y: 100 }, data: { label: 'Species 2', onLabelChange: () => {}, color: '#ffd6e0', initial: '' }, type: 'protein'},
 ];
 
-// const initialEdges: AppEdge[] = [{ id: 'Na_Nb', source: 'Na', target: 'Nb' , markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20 }, animated: true, type: 'reaction', data: { label: 'test2', toggleDrawer: () => {}, rate_law: ''}, }];
- 
 const initialEdges: AppEdge[] = [{ id: 'Na_Nb', source: 'Na', target: 'Nb' , animated: true, type: 'reaction', data: { label: 'test2', toggleDrawer: () => {}, rate_law: ''}, }];
+
+const initialSpecies: species[] = [
+  { id: 'Na', name: 'Click to edit', initial: 10 },
+  { id: 'Nb', name: 'Species 2', initial: 0 },
+];
+
+const initialReactions: reactions[] = [
+  { id: 'Na_Nb', name: 'Reaction 1', from: ['Na'], to: ['Nb'] },
+];
 
 type AppState = {
   visualNodes: AppNode[];
   visualEdges: AppEdge[];
 
-  // species: [];
-  // reactions: [];
+  species: species[];
+  reactions: reactions[];
 
   onNodesChange: OnNodesChange<AppNode>;
   onEdgesChange: OnEdgesChange<AppEdge>;
@@ -37,6 +57,9 @@ type AppState = {
 const useStore = create<AppState>((set, get) => ({
     visualNodes: initialNodes,
     visualEdges: initialEdges,
+    species: initialSpecies,
+    reactions: initialReactions,
+
     onNodesChange: (changes) => {
       set({
         visualNodes: applyNodeChanges(changes, get().visualNodes),
